13e502c70a1a6e67ac8acfd9b99300a8ad556e89
darelljay <darelljason1@gmail.com>
2024-01-14 20:35:24 +0900

    build mui 설치 완료

---
+++ promodoro-app/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/header"
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <div>
+      hello world
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ promodoro-app/src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main: '#EF476F', 
+        },
+        secondary:{
+            main: '#118AB2',
+        },
+        text:{
+            primary: '#2A3132',
+            secondary: '#5E7480',
+        },
+    },
+});
+
+export default theme;
